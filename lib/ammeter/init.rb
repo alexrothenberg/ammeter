e0770b67426d1e641575c37347167c469f5031e7
Josef Stribny <jstribny@redhat.com>
2013-09-23 12:16:03 +0200

    Require Rails first since rspec-rails use the Rails constant

--- lib/ammeter/init.rb
+++ lib/ammeter/init.rb
@@ -2,9 +2,9 @@ require 'rspec/core'
 if RSpec::Core::Version::STRING < '3'
   require 'ammeter/rspec/rspec_2_compatibility'  # if rspec2
 end
+require 'rails'
 require 'ammeter/rspec/generator/example.rb'
 require 'ammeter/rspec/generator/matchers.rb'
-require 'rails'
 
 if Rails.respond_to?(:application) && Rails.application.respond_to?(:load_generators)
   Rails.application.load_generators
