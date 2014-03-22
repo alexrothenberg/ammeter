5e7ec3516c89713603e83b82ed71b4f1df06e089
Alex Rothenberg <alex@alexrothenberg.com>
2014-03-22 10:27:51 -0400

    made a spec pending on ruby 1.9.3. Not sure if it is a real bug or not :(

--- lib/ammeter/rspec/generator/matchers/be_a_migration.rb
+++ lib/ammeter/rspec/generator/matchers/be_a_migration.rb
@@ -1,6 +1,11 @@
 RSpec::Matchers.define :be_a_migration do
   match do |file_path|
     dirname, file_name = File.dirname(file_path), File.basename(file_path)
+
+    # Without this hacky next line we get
+    # TypeError: can't convert MatchData into String
+    StringIO.new.puts 'hi' if RUBY_VERSION == '1.9.3'
+
     if file_name.match(/\d+_.*\.rb/)
       migration_file_path = file_path
     else
