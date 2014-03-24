20711d28169822f7bd72b0e99c2b724dd7f5b1dc
Alex Rothenberg <alex@alexrothenberg.com>
2014-03-24 08:27:09 -0400

    Fix for ruby 1.9.3
    Change .match to =~ as suggested by @cupakromer in https://github.com/alexrothenberg/ammeter/commit/5e7ec3516c89713603e83b82ed71b4f1df06e089#commitcomment-5770325

--- lib/ammeter/rspec/generator/matchers/be_a_migration.rb
+++ lib/ammeter/rspec/generator/matchers/be_a_migration.rb
@@ -2,11 +2,7 @@ RSpec::Matchers.define :be_a_migration do
   match do |file_path|
     dirname, file_name = File.dirname(file_path), File.basename(file_path)
 
-    # Without this hacky next line we get
-    # TypeError: can't convert MatchData into String
-    StringIO.new.puts 'hi' if RUBY_VERSION == '1.9.3'
-
-    if file_name.match(/\d+_.*\.rb/)
+    if file_name =~ /\d+_.*\.rb/
       migration_file_path = file_path
     else
       migration_file_path = Dir.glob("#{dirname}/[0-9]*_*.rb").grep(/\d+_#{file_name}$/).first
