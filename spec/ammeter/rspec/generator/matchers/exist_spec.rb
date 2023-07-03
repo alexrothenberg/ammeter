950c8d07e2e8cd3d3ef4bfcd2e09906c5c04d12c
Adam Stegman <astegman@onemedical.com>
2023-07-03 13:29:32 -0500

    Deprecate the custom exist matcher
    Prefer the one built-in by RSpec instead.
    Fall back to the built-in matcher for default behavior when given a filename.

--- spec/ammeter/rspec/generator/matchers/exist_spec.rb
+++ spec/ammeter/rspec/generator/matchers/exist_spec.rb
@@ -5,9 +5,15 @@ describe "exist" do
     allow(File).to receive(:exist?).with('/some/file/path').and_return(true)
     expect('/some/file/path').to exist
   end
+
   it 'fails when the file does not exist' do
     allow(File).to receive(:exist?).with('/some/file/path').and_return(false)
     expect('/some/file/path').to_not exist
   end
 
+  it 'maintains default RSpec behavior' do
+    path = Pathname.new('/some/file/path')
+    allow(path).to receive(:exist?).and_return(true)
+    expect(path).to exist
+  end
 end
